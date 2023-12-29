299476a1ee37274e7f517d51e9bd121b7c70aecf
Lucas da Silva <lucasmacabu1920@gmail.com>
2023-12-29 00:41:18 -0300

    adicionando configuração para deploy na vercel

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 const PORT = process.env.PORT || 9001;
 const app = express();
+app.use(express.json());
 
 app.get('/home', (req, res) => {
-  res.send('Home')
+  res.json('Home')
+})
+
+app.get('/about', (req, res) => {
+  res.json('About')
 })
 
 app.listen(PORT, () => {
